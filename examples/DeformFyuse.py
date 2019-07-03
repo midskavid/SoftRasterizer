19ffbc0c93245789c0ac0a4791a5232671052089
mkavidayal-fy <mkavidayal@fyusion.com>
2019-07-03 14:46:29 -0700

    Bug Fixes in code

--- examples/DeformFyuse.py
+++ examples/DeformFyuse.py
@@ -1,12 +1,14 @@
 import torch
+import os
+import tqdm
+import imageio
+import argparse
+import json
+import glob
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-import os
-import tqdm
-import numpy as np
-import imageio
-import argparse
 
 import soft_renderer as sr
 
@@ -38,6 +40,7 @@ class PoseReader():
             K = [
                 [intrinsics[0], intrinsics[2], intrinsics[3] - 0.5], [0, intrinsics[1], intrinsics[4] - 0.5], [0, 0, 1]
             ]
+
             # K would be [fx skew cx; 0 fy cy; 0 0 1]
             # intrinsicsVector contains [fx,fy,skew,cx,cy]
             transforms['K'] = torch.Tensor(K)
@@ -83,13 +86,12 @@ class Model(nn.Module):
         laplacian_loss = self.laplacian_loss(vertices).mean()
         flatten_loss = self.flatten_loss(vertices).mean()
 
-        return sr.Mesh(vertices.repeat(batch_size, 1, 1), 
-                       self.faces.repeat(batch_size, 1, 1)), laplacian_loss, flatten_loss
+        return sr.Mesh(vertices.repeat(batch_size, 1, 1), self.faces.repeat(batch_size, 1, 1)), laplacian_loss, flatten_loss
 
 
 def SilhouetteLoss(predict, target):
     dims = tuple(range(predict.ndimension())[1:])
-    intersect = (predict * target).sum(dims)
+    intersect = (predict * target).sum(dims) + 1e-6
     union = (predict + target - predict * target).sum(dims) + 1e-6
     return 1. - (intersect / union).sum() / intersect.nelement()
 
@@ -107,8 +109,6 @@ def main():
     model = Model(args.template_mesh).cuda()
     optimizer = torch.optim.Adam(model.parameters(), 0.01, betas=(0.5, 0.99))
 
-    renderer = sr.SoftRenderer(image_size=256, sigma_val=1e-4, aggr_func_rgb='hard', 
-                               camera_mode='projection', viewing_angle=42.5)
 
     #Read in data ./ 
 
@@ -119,31 +119,35 @@ def main():
     projMatrices = []
 
     for ii in range(numFrames) :
-        idx = poseData.keys()[ii]
-        projMat = torch.mm(poseData.poses[ii]['K'],poseData.poses[ii]['Rt'][0:3,:])
-        imgGt = imageio.read(args.data_dir+'{0:09d}'.format(idx))
+        # print (poseData.poses.keys())
+        idx = list(poseData.poses)[ii]
+        projMat = torch.mm(poseData.poses[idx]['K'],poseData.poses[idx]['Rt'][0:3,:])
+        # print (projMat.shape)
+        imgGt = imageio.imread(args.data_dir+'Gt/{0:09d}'.format(idx)+'.jpg').astype('float32') / 255.0
         # swap axes ??
         imagesGT.append(imgGt)
         projMatrices.append(projMat)
 
 
+    projMatrices = torch.stack(projMatrices)
+    projMatrices = projMatrices.cuda()
+    
+    renderer = sr.SoftRenderer(image_size=256, sigma_val=1e-4, aggr_func_rgb='hard', camera_mode='projection', P=projMatrices)#, viewing_angle=12.5)
 
     loop = tqdm.tqdm(list(range(0, 2000)))
     writer = imageio.get_writer(os.path.join(args.output_dir, 'deform.gif'), mode='I')
-    images_gt = torch.from_numpy(imagesGT).cuda()
-    projMatrices = torch.tensor(projMatrices).cuda()
+    images_gt = torch.from_numpy(np.array(imagesGT)).cuda()
     
-    for i in loop:
-        
 
-        mesh, laplacian_loss, flatten_loss = model(args.batch_size)
+    for i in loop:
+        mesh, laplacian_loss, flatten_loss = model(numFrames)
         images_pred = renderer.render_mesh(mesh)
 
+        #print(images_pred.shape)
         # optimize mesh with silhouette reprojection error and 
         # geometry constraints
-        loss = neg_iou_loss(images_pred[:, 3], images_gt[:, 3]) + \
-               0.03 * laplacian_loss + \
-               0.0003 * flatten_loss
+        #print (SilhouetteLoss(images_pred[:,3], images_gt), laplacian_loss, flatten_loss)
+        loss = SilhouetteLoss(images_pred[:,3], images_gt) + 0.03 * laplacian_loss + 0.0003 * flatten_loss
 
         loop.set_description('Loss: %.4f'%(loss.item()))
 
@@ -151,13 +155,12 @@ def main():
         loss.backward()
         optimizer.step()
 
-        if i % 100 == 0:
+        if i % 10 == 0:
             image = images_pred.detach().cpu().numpy()[0].transpose((1, 2, 0))
             writer.append_data((255*image).astype(np.uint8))
             imageio.imsave(os.path.join(args.output_dir, 'deform_%05d.png'%i), (255*image[..., -1]).astype(np.uint8))
-
-    # save optimized mesh
-    model(1)[0].save_obj(os.path.join(args.output_dir, 'plane.obj'), save_texture=False)
+            # save optimized mesh
+            model(1)[0].save_obj(os.path.join(args.output_dir, 'plane.obj'), save_texture=False)
 
 
 if __name__ == '__main__':
